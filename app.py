d7ed8779c215a6038d29f7085723b7f2a1939455
Luca <lucarrcosta@gmail.com>
2026-02-14 17:45:53 -0300

    implementing: crud implementation

--- app.py
+++ app.py
@@ -1,14 +1,78 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from models.task import Task
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello World!"
+tasks = []
+task_id_control = 1
 
-@app.route("/about")
-def about():
-    return "About page"
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    global task_id_control
+    data = request.get_json()
+    new_task = Task(id=task_id_control, title=data['title'], description=data.get("description", ""))
+    task_id_control += 1
+    tasks.append(new_task)
+    print(tasks)
+    return jsonify({"message":"Nova tarefa criada com sucesso"})
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    task_list = [task.to_dict() for task in tasks]
+    output = {
+        "tasks": task_list,
+        "total_tasks": len(task_list)
+    }
+    return jsonify(output)  
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+
+    if task is None:
+        return jsonify({"message": "Tarefa não encontrada"}), 404
+
+    return jsonify(task.to_dict())
+
+@app.route('/tasks/<int:id>', methods=['PUT'])
+def update_task(id):
+    print("Atualizando tarefa com id:", id)
+    data = request.get_json()
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+
+    if task is None:
+        return jsonify({"message": "Tarefa não encontrada"}), 404
+
+    task.title = data.get('title', task.title)
+    task.description = data.get('description', task.description)
+    task.completed = data.get('completed', task.completed)
+    print("Tarefa atualizada:", task.to_dict())
+    return jsonify({"message": "Tarefa atualizada com sucesso"})
+
+
+@app.route('/tasks/<int:id>', methods=['DELETE'])
+def delete_task(id):
+    global tasks
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+            break
+
+    if task is None:
+        return jsonify({"message": "Tarefa não encontrada"}), 404
+
+    tasks.remove(task)
+    print("Tarefa deletada:", task.to_dict())
+    return jsonify({"message": "Tarefa deletada com sucesso"})
 
 if __name__ == "__main__":
     app.run(debug=True)
